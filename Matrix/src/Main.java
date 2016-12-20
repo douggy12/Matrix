b97279008a6f0a94071bde73bb5ee9203e0ed93f
allan <allan@10.0.2.15>
2016-12-20 11:58:25 +0100

    matrix new timer

--- Matrix/src/Main.java
+++ Matrix/src/Main.java
@@ -1,16 +1,28 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Pixel[] tableau = new Pixel[10];
-		for(int i=0; i<10; i++){
-			tableau[i]=new Pixel();
-		}
-		for(int i = 0; i < 10; i++){
-			if (tableau[i].getTaille()<0){tableau[i]=new Pixel(); System.out.println(tableau[i].toString());}
-			else System.out.println(tableau[i].toString());
-		}
-		
+		Timer timer = new Timer();
+
+
+		timer.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				for(int i=0; i<10; i++){
+					tableau[i]=new Pixel();
+				}
+				for(int i = 0; i < 10; i++){
+					if (tableau[i].getTaille()<0){tableau[i]=new Pixel(); System.out.print(tableau[i].toString());}
+					else System.out.print(tableau[i].toString());
+				}
+			}
+
+
+		}, 0,500);
+
 	}
-	  
 }
