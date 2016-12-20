bc5ee4415197985d27c156a7981a03f68447582a
allan <allan@10.0.2.15>
2016-12-20 11:42:14 +0100

    main

--- Matrix/src/Main.java
+++ Matrix/src/Main.java
@@ -2,8 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Pixel[] tableau = new Pixel[10];
+		for(int i=0; i<10; i++){
+			tableau[i]=new Pixel();
+		}
+		for(int i = 0; i < 10; i++){
+			if (tableau[i].getTaille()<0){tableau[i]=new Pixel(); System.out.println(tableau[i].toString());}
+			else System.out.println(tableau[i].toString());
+		}
+		
 	}
-
+	
 }
