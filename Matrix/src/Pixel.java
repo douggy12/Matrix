71b5e7970fea0181e79e43a2a121e8258b7ceeac
dmett <dmett@10.0.2.15>
2016-12-20 11:34:25 +0100

    mon objet pixel

--- Matrix/src/Pixel.java
+++ Matrix/src/Pixel.java
@@ -1,4 +1,23 @@
 
 public class Pixel {
+	private int bool;
+	private int taille;
+	
+	public Pixel(){
+		bool = (int)Math.random();
+		taille = (int)Math.random()*10-1;
+	}
+
+	public int getTaille() {
+		return taille;
+	}
+
+	@Override
+	public String toString() {
+		if (bool==1)return (((int)Math.random()*10-1)+"");
+		else return (" ");
+	}
+	
+	
 
 }
